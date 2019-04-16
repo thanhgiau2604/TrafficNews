f17df96787bd2717eea3fed01a7ed6886473e461
thanhgiau2604 <thanhgiau2604@gmail.com>
2019-04-16 23:32:24 +0700

    Up

---
+++ WebNewsV2/src/main/java/myweb/WebNewsV2Application.java
@@ -0,0 +1,13 @@
+package myweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import  org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@SpringBootApplication
+public class WebNewsV2Application {
+	public static void main(String[] args) {
+		SpringApplication.run(WebNewsV2Application.class, args);
+	}
+}
